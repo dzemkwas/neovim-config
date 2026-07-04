ee9087cfea5d87733b51181ddc8c146727d23170
Kacper <zacharek.kacper12@gmail.com>
2026-07-04 17:32:20 +0200

    rebase

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     require('nvim-treesitter').setup {
       install_dir = vim.fn.stdpath('data') .. '/site'
     }
-    require('nvim-treesitter').install { 'c','cpp', 'lua', 'rust', 'javascript', 'zig' }
+    require('nvim-treesitter').install { 'c','cpp', 'lua', 'rust', 'javascript', 'zig', 'bash' }
     end
   }
 }
